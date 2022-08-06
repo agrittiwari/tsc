3e5f5f73c9742042d6197a7b4f4400641374ed04
Agrit Tiwari <agrit1408@gmail.com>
2022-08-06 14:55:28 +0530

    define function type

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,8 @@
 var a = '1';
+var hello = 'world';
+//hello=[]  
+//comment out to see error  
+//functions
+var printName = function (name, surname, age) {
+    return name + '' + surname + ' is' + age + ' years old';
+};
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,18 @@ let hello: string = 'world';
 
 //hello=[]  
 //comment out to see error  
+
+
+
+//functions
+
+
+const printName =(name:string, surname:string, age:number) =>{
+    return name + '' + surname +' is' + age + ' years old'
+}
+
+//defining a function
+
+const printSum =(num1:number, num2:number):number =>{
+    return num1 + num2;
+}
